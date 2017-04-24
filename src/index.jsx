1a150b1b003f7ee653aa734050b42e379157e3af
Narasimha <narasimha@codebrahma.com>
2017-04-24 12:15:40 +0530

    Added redux and created store with initialState

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,15 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import manageComponents from './reducers/index.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+const store = createStore(manageComponents);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('react-root')
+);
---
+++ src/reducers/index.jsx
@@ -0,0 +1,12 @@
+const initialState = {
+	selected: '',
+	checked: '',
+	text: ''
+}
+
+export default function manageComponents(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }	
+}
